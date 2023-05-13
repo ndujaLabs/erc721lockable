f1ce174160ee5dd42d5f5c42c778545cd55a1d6d
Francesco Sullo <sullof@gmail.com>
2023-05-13 15:11:05 -0700

    Remove bin

--- bin/flatten.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# must be run from the root
-
-npx hardhat flatten contracts/$1.sol > ./$1-flatten.sol
--- bin/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# must be run from the root
-
-#rm -rf artifacts
-#rm -rf cache
-npx hardhat compile
-
-CONTRACT=$1 GAS_LIMIT=$3 npx hardhat run scripts/upgrade.js --network $2
